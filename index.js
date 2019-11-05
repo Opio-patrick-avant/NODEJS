0891c0312ba14f1cafd9e9fef7f28fb4b92df5d1
avant <opioodoki@gmail.com>
2019-11-05 09:37:00 +0300

    3rdadd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+/*console.log('Hello world');*/
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log('listening on 3000')
+})
+
