fa517073b1a544c7085b9e464b3ab2ff8ab47217
avant <opioodoki@gmail.com>
2019-11-06 09:17:26 +0300

    2ndpus

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function(){
-    console.log('listening on 3000')
+app.listen(3001, function(){
+    console.log('listening on 3001')
+})
+
+app.get('/',(req,res)=>{
+    res.send(`Hello opio`)
+    
 })
 
